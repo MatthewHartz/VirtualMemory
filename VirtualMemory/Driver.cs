6373cb651c6078f3f142d5e6f0e265b1e25a2edb
rvbmrdonut <hartzm@uci.edu>
2015-03-01 23:39:39 -0800

    saved

--- VirtualMemory/Driver.cs
+++ VirtualMemory/Driver.cs
@@ -11,12 +11,10 @@ namespace ProcessAndResourceManager
     {
         static void Main(string[] args)
         {
-            var manager = ProcessAndResourceManager.Instance;
+            //var manager = ProcessAndResourceManager.Instance;
             var sb = new StringBuilder(); // Will contain file output
             var stream = new StreamReader("C:\\Users\\Matthew\\Desktop\\input.txt");
             String line;
-
-            sb.Append(manager.GetRunningProcess());
 
             while ((line = stream.ReadLine()) != null)
             {
@@ -24,43 +22,7 @@ namespace ProcessAndResourceManager
 
                 try
                 {
-                    switch (tokens[0].ToLower())
-                    {
-                        case "init":
-                            manager.Initialize();
-                            sb.AppendLine().AppendLine();
-                            sb.Append(manager.GetRunningProcess());
-                            break;
-                        case "quit":
-                            manager.Quit();
-                            break;
-                        case "cr":
-                            manager.Create(Char.Parse(tokens[1]), Int32.Parse(tokens[2]));
-                            sb.Append(" " + manager.GetRunningProcess());
-                            break;
-                        case "de":
-                            manager.Destroy(Char.Parse(tokens[1]));
-                            sb.Append(" " + manager.GetRunningProcess());
-                            break;
-                        case "req":
-                            manager.Request(tokens[1], Int32.Parse(tokens[2]));
-                            sb.Append(" " + manager.GetRunningProcess());
-                            break;
-                        case "rel":
-                            manager.Release(tokens[1], Int32.Parse(tokens[2]));
-                            sb.Append(" " + manager.GetRunningProcess());
-                            break;
-                        case "to":
-                            manager.Timeout();
-                            sb.Append(" " + manager.GetRunningProcess());
-                            break;
-                        case "":
-                            break;
-                        case "#":
-                            break;
-                        default:
-                            throw new Exception("invalid operation");
-                    }
+                    
 
                 }
                 catch (Exception e)
